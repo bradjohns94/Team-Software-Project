6d6f21edb31f4f8da44e122c6802522b0a553b4a
Bradley Johns <bradjohns94@gmail.com>
2014-01-23 23:38:27 +0000

    Delete test.sh

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo -e "Tell me your name please!: \c"
-read name
-echo -e "Tell me your favorite color please! \c"
-read color
-echo "Your name is $name"
-echo "Your favorite color is $color"
