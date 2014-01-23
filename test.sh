64870a173dcaf51dd80b6662b27f5e85ffae2f5e
Bradley Johns <BradJohns94@gmail.com>
2014-01-23 18:37:10 +0000

    this is a test script that does nothing important

---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo -e "Tell me your name please!: \c"
+read name
+echo -e "Tell me your favorite color please! \c"
+read color
+echo "Your name is $name"
+echo "Your favorite color is $color"
